e10393d49ed1c19925d7ddbbae06f527027d1bb3
vitalya <vitalay.efimov@gmail.com>
2025-04-29 20:13:27 +0500

    task1

--- unit_08.js
+++ unit_08.js
@@ -3,11 +3,13 @@
 //     1 2 3 4 5 6 7 8 9 ... 49 50
 // от 1 до 50 включительно. Разделитель - пробел. Задача решается с помощью цикла.
 
-
+ let out = document.querySelector('.out-1')
 function t1() {
-
-}
-
+  let i = 0
+  while (i <= 50) {
+    out.innerHTML += i + ' ';
+    i++;
+  }}
 document.querySelector('.b-1').onclick = t1;
 
 //  Task 2
