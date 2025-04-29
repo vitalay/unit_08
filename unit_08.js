f905d7ecb33c25b6b49ec513792fe5d80bc5154c
vitalya <vitalay.efimov@gmail.com>
2025-04-29 20:24:22 +0500

    task2

--- unit_08.js
+++ unit_08.js
@@ -16,9 +16,16 @@ document.querySelector('.b-1').onclick = t1;
 // Кнопка b-2 запускает функцию t2. Функция должна выводить в out-2 строку вида:
 //     2 4 6 ... 122
 // от 2 до 122 c шагом 2. Разделитель - пробел. Задача решается с помощью цикла  while.
-
+let out2 = document.querySelector('.out-2')
 function t2() {
-
+   let i = 2;
+   while (i <= 122) {
+     if (i % 2 === 0) {
+         out2.innerHTML += i + ' ';
+     }
+    i++;
+    console.log(i);
+   }
 }
 
 document.querySelector('.b-2').onclick = t2;
